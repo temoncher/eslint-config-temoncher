9a29f2415c21ec2135065bb4caaa2fbe6f40ed73
temoncher <temoncher@yandex.ru>
2020-11-23 17:38:32 +0300

    fix react package

--- react.js
+++ react.js
@@ -14,9 +14,9 @@ module.exports = {
     'plugin:import/react',
     'airbnb',
     'airbnb/hooks',
-    './base',
   ],
   rules: {
+    // [Plugin:React](https://github.com/yannickcr/eslint-plugin-react)
     'react/jsx-filename-extension': [
       2,
       { extensions: ['.js', '.jsx', '.tsx'] },
